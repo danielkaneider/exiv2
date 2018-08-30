f3e3bf3369fc16225023d985df34e826d0b49e56
Dan Čermák <dan.cermak@cgc-instruments.com>
2018-08-30 23:41:48 +0200

    Add second reproducer for #159 to the test suite
    This fixes #159

--- tests/bugfixes/github/test_issue_159.py
+++ tests/bugfixes/github/test_issue_159.py
@@ -3,17 +3,46 @@
 import system_tests
 
 
-class TestFirstPoC(metaclass=system_tests.CaseMeta):
+class TestPoCs(metaclass=system_tests.CaseMeta):
     """
-    Regression test for the first bug described in:
+    Regression test for the bugs described in:
     https://github.com/Exiv2/exiv2/issues/159
     """
     url = "https://github.com/Exiv2/exiv2/issues/159"
 
-    filename = "$data_path/printStructure"
-    commands = ["$exiv2 " + filename]
+    files = [
+        system_tests.path("$data_path/printStructure" + c) for c in ('', '2')
+    ]
+    commands = ["$exiv2 " + fname for fname in files]
     stdout = [
-        """File name       : $filename
+        """File name       : """ + files[0] + """
+File size       : 12357 Bytes
+MIME type       : image/tiff
+Image size      : 0 x 0
+Camera make     : 
+Camera model    : 
+Image timestamp : 
+Image number    : 
+Exposure time   : 
+Aperture        : 
+Exposure bias   : 
+Flash           : 
+Flash bias      : 
+Focal length    : 
+Subject distance: 
+ISO speed       : 
+Exposure mode   : 
+Metering mode   : 
+Macro mode      : 
+Image quality   : 
+Exif Resolution : 
+White balance   : 
+Thumbnail       : None
+Copyright       : 
+Exif comment    : 
+
+""",
+        """File name       : """ + files[1] + """
 File size       : 12357 Bytes
 MIME type       : image/tiff
 Image size      : 0 x 0
@@ -41,11 +70,7 @@ Exif comment    :
 
 """
     ]
-    stderr = [""]
-    retval = [0]
+    stderr = [""] * 2
+    retval = [0] * 2
 
     compare_stderr = system_tests.check_no_ASAN_UBSAN_errors
-
-
-# todo:
-# class TestSecondPoC(system_tests.Case):
